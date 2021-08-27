9d111b38b3700b2f5d855c2c5e038d3674561141
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-08-27 19:37:41 +0300

    add HW3 mongoDb

---
+++ model/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+const uriDb = process.env.URI_DB
+
+const db = mongoose.connect(uriDb, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, poolSize: 5 })
+
+mongoose.connection.on('connected', () => {
+    console.log('Connection is open')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Mongoose error: ${err.message}`);
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log(`Mongoose disconnected`);
+})
+
+process.on('SIGINT', async () => {
+    mongoose.connection.close(() => {
+         console.log('Connection to DB terminated')
+    process.exit(1)
+    })
+})
+
+module.exports = db
