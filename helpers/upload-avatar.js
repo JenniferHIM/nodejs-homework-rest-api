0dbaf5d375fdd7e02b829bec44caf150938da6c0
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-09-13 17:42:20 +0300

    Add uploads

--- helpers/upload-avatar.js
+++ helpers/upload-avatar.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
         cb(null, UPLOAD_DIR)
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname)
+        cb(null, `${Date.now().toString()}-${ file.originalname}`);
     }
 })
 
